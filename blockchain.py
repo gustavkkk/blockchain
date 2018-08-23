c56e76e93c74e54c0a21c705e07eb49ebbb967c3
frank K <gustav0125@outlook.com>
2018-08-23 18:01:02 +0800

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -31,7 +31,7 @@ def next_block(last_block):
   return Block(this_index, this_timestamp, this_data, this_hash)
   
   
-def main()
+def init()
   # Create the blockchain and add the genesis block
   blockchain = [create_genesis_block()]
   previous_block = blockchain[0]
@@ -49,5 +49,96 @@ def main()
     print "Block #{} has been added to the blockchain!".format(block_to_add.index)
     print "Hash: {}\n".format(block_to_add.hash) 
 
+from flask import Flask
+from flask import request
+node = Flask(__name__)
+
+# Store the transactions that
+# this node has in a list
+this_nodes_transactions = []
+
+@node.route('/txion', methods=['POST'])
+def transaction():
+  if request.method == 'POST':
+    # On each new POST request,
+    # we extract the transaction data
+    new_txion = request.get_json()
+    # Then we add the transaction to our list
+    this_nodes_transactions.append(new_txion)
+    # Because the transaction was successfully
+    # submitted, we log it to our console
+    print "New transaction"
+    print "FROM: {}".format(new_txion['from'])
+    print "TO: {}".format(new_txion['to'])
+    print "AMOUNT: {}\n".format(new_txion['amount'])
+    # Then we let the client know it worked out
+    return "Transaction submission successful\n"
+
+node.run()    
+
+# ...blockchain
+# ...Block class definition
+
+miner_address = "q3nf394hjg-random-miner-address-34nf3i4nflkn3oi"
+
+def proof_of_work(last_proof):
+  # Create a variable that we will use to find
+  # our next proof of work
+  incrementor = last_proof + 1
+  # Keep incrementing the incrementor until
+  # it's equal to a number divisible by 9
+  # and the proof of work of the previous
+  # block in the chain
+  while not (incrementor % 9 == 0 and incrementor % last_proof == 0):
+    incrementor += 1
+  # Once that number is found,
+  # we can return it as a proof
+  # of our work
+  return incrementor
+
+@node.route('/mine', methods = ['GET'])
+def mine():
+  # Get the last proof of work
+  last_block = blockchain[len(blockchain) - 1]
+  last_proof = last_block.data['proof-of-work']
+  # Find the proof of work for
+  # the current block being mined
+  # Note: The program will hang here until a new
+  #       proof of work is found
+  proof = proof_of_work(last_proof)
+  # Once we find a valid proof of work,
+  # we know we can mine a block so 
+  # we reward the miner by adding a transaction
+  this_nodes_transactions.append(
+    { "from": "network", "to": miner_address, "amount": 1 }
+  )
+  # Now we can gather the data needed
+  # to create the new block
+  new_block_data = {
+    "proof-of-work": proof,
+    "transactions": list(this_nodes_transactions)
+  }
+  new_block_index = last_block.index + 1
+  new_block_timestamp = this_timestamp = date.datetime.now()
+  last_block_hash = last_block.hash
+  # Empty transaction list
+  this_nodes_transactions[:] = []
+  # Now create the
+  # new block!
+  mined_block = Block(
+    new_block_index,
+    new_block_timestamp,
+    new_block_data,
+    last_block_hash
+  )
+  blockchain.append(mined_block)
+  # Let the client know we mined a block
+  return json.dumps({
+      "index": new_block_index,
+      "timestamp": str(new_block_timestamp),
+      "data": new_block_data,
+      "hash": last_block_hash
+  }) + "\n"
+
 if __name__ == "__main__":
-  main()
+  init()
