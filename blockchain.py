26aff54a34a031c3dee3c0b76cebba57925b36bd
frank K <gustav0125@outlook.com>
2018-08-23 18:43:29 +0800

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,7 @@
+# @ref
+# https://medium.com/crypto-currently/lets-build-the-tiniest-blockchain-e70965a248b
+# https://medium.com/crypto-currently/lets-make-the-tiniest-blockchain-bigger-ac360a328f4d
+#
 import hashlib as hasher
 
 class Block:
